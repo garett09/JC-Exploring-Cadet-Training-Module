f1992bb0df74fedcac86bf79bee26ea0d5ebfe10
Adrian Garett Sian <adriansian@gmail.com>
2025-08-13 22:43:27 +0800

    add files

--- src/main/java/org/example/AdvanceRecon.java
+++ src/main/java/org/example/AdvanceRecon.java
@@ -1,4 +1,6 @@
 package org.example;
 
-public interface AdvanceRecon {
+public interface AdvanceRecon extends VisualRecon, SignalIntel {
+    // This interface is a combination of two contracts.
+    // It doesn't need a body, it just aggregates other interfaces.
 }
